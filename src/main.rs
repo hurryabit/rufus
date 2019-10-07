051350036925c8f6ded769068b9855cd8925ad71
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-10-07 11:06:40 +0200

    Refactor tests

--- src/main.rs
+++ src/main.rs
@@ -58,28 +58,36 @@ fn main() {
     rl.save_history(HISTORY_FILE).unwrap();
 }
 
-#[test]
-fn test() {
-    use cek::*;
-    let parser = parser::ExprParser::new();
+#[cfg(test)]
+mod tests {
+    fn integration_test(expected: i64, expr: &str) {
+        use crate::cek::*;
+        use crate::parser::ExprParser;
+        let parser = ExprParser::new();
+        let expr = parser
+            .parse(expr)
+            .unwrap()
+            .index()
+            .unwrap();
+        assert_eq!(State::init(&expr).run().as_i64(), expected);
+    }
 
-    let expr1 = parser
-        .parse(
-            "
+    #[test]
+    fn twice() {
+        integration_test(162, "
             let t = 3;
             let f = |x| (t * x);
             let twice = |f, x| f(f(x));
-            twice(|x| twice(f, x), 2)",
-        )
-        .unwrap()
-        .index()
-        .unwrap();
-    assert_eq!(State::init(&expr1).run().as_i64(), 162);
+            twice(|x| twice(f, x), 2)");
+    }
 
-    let expr2 = parser
-        .parse("let x = 1; let y = {let z = 2; z}; x")
-        .unwrap()
-        .index()
-        .unwrap();
-    assert_eq!(State::init(&expr2).run().as_i64(), 1);
+    #[test]
+    fn nested_let() {
+        integration_test(1, "let x = 1; let y = {let z = 2; z}; x");
+    }
+
+    #[test]
+    fn simple_lambda() {
+        integration_test(5, "let x = 1; let y = 2; let f = |z| (x+z); f(4)");
+    }
 }
