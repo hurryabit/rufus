711ca24ab2254691737e92d8c2c5a236a0672ab3
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-10-07 18:36:54 +0200

    Add header and footer to main screen

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rufus
-An educational experiment to implement a functional programming language in Rust
+An experiment about a CEK machine implemented in Rust,
+compiled to Web Assembly and made alive via Typescript + React.
+
+A running instance can be found at https://hurryabit.github.io/rufus.
--- rufus-ui/src/App.tsx
+++ rufus-ui/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Jumbotron, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+const EDITOR_ROWS: number = 12;
 
 type Props = {};
 
@@ -68,56 +70,76 @@ class App extends React.Component<Props, State> {
   render() {
     const state = this.state;
     return (
-      <Container>
-        <Row>
-          <Col xs={8}>
-            <Form>
-              <Form.Group>
-                <Form.Label>Program:</Form.Label>
-                <textarea
-                  className='form-control'
-                  rows={20}
-                  value={state.program}
-                  onChange={this.handleProgramChange}
-                  onKeyDown={this.handleProgramKeyDown}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>&nbsp;</Form.Label>
-                <Button
-                  variant="primary"
-                  className='btn-block'
-                  type="submit"
-                  onClick={this.handleRunClick}
-                >
-                  Run
-                </Button>
-              </Form.Group>
-            </Form>
-          </Col>
-          <Col xs={4}>
-            <Form>
-              <Form.Group>
-                <Form.Label>Output:</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  readOnly
-                  rows={20}
-                  value={state.output}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Result:</Form.Label>
-                <Form.Control
-                  type='text'
-                  readOnly
-                  value={state.result}
-                />
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+      <React.Fragment>
+        <Container>
+          <Row>
+            <Col>
+              <Jumbotron>
+                <h1>rufus</h1>
+                <p>
+                  An experiment about a CEK machine implemented in Rust,
+                  compiled to Web Assembly and made alive via Typescript + React.
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={8}>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Program:</Form.Label>
+                  <textarea
+                    className='form-control'
+                    rows={EDITOR_ROWS}
+                    value={state.program}
+                    onChange={this.handleProgramChange}
+                    onKeyDown={this.handleProgramKeyDown}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>&nbsp;</Form.Label>
+                  <Button
+                    variant="primary"
+                    className='btn-block'
+                    type="submit"
+                    onClick={this.handleRunClick}
+                  >
+                    Run
+                  </Button>
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col xs={4}>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Output:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    readOnly
+                    rows={EDITOR_ROWS}
+                    value={state.output}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Result:</Form.Label>
+                  <Form.Control
+                    type='text'
+                    readOnly
+                    value={state.result}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+        <footer className="fixed-bottom">
+          <div className="footer-copyright text-center py-1">
+            <span className="text-muted">
+              © 2019 <a href="https://github.com/hurryabit/rufus" target="blank">Martin Huschenbett</a>
+            </span>
+          </div>
+        </footer>
+      </React.Fragment>
     );
   }
 }
