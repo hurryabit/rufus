beabd11e35018dd6a44a1031ae4cdd77f7c12f6a
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-10-07 23:32:09 +0200

    Style with bulma

--- rufus-ui/src/App.tsx
+++ rufus-ui/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, Col, Container, Form, Jumbotron, Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bulma';
 import './App.css';
 
-const EDITOR_ROWS: number = 12;
+const EDITOR_ROWS: number = 15;
+
+const DEFAULT_PROGRAM: string =
+`let twice = |f, x| { f(f(x)) };
+let inc = |x| { x + 1 };
+twice(inc, 0)`;
 
 type Props = {};
 
@@ -19,7 +23,7 @@ class App extends React.Component<Props, State> {
     super(props);
     this.state = {
       wasm: null,
-      program: '',
+      program: DEFAULT_PROGRAM,
       output: '',
       result: '',
     };
@@ -31,14 +35,14 @@ class App extends React.Component<Props, State> {
   loadWasm = async () => {
     try {
       const wasm = await import('rufus');
-      this.setState({wasm});
-    } catch(err) {
+      this.setState({ wasm });
+    } catch (err) {
       console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
     }
   }
 
   handleProgramChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({program: event.currentTarget.value})
+    this.setState({ program: event.currentTarget.value })
   }
 
   handleProgramKeyDown = (event: React.KeyboardEvent) => {
@@ -59,7 +63,7 @@ class App extends React.Component<Props, State> {
     const value = result.get_value();
     switch (status) {
       case wasm.ExecResultStatus.Ok:
-        this.setState({result: value});
+        this.setState({ result: value });
         break;
       case wasm.ExecResultStatus.Err:
         alert(value);
@@ -71,72 +75,77 @@ class App extends React.Component<Props, State> {
     const state = this.state;
     return (
       <React.Fragment>
-        <Container>
-          <Row>
-            <Col>
-              <Jumbotron>
-                <h1>rufus</h1>
-                <p>
-                  An experiment about a CEK machine implemented in Rust,
-                  compiled to Web Assembly and made alive via Typescript + React.
-                </p>
-              </Jumbotron>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={8}>
-              <Form>
-                <Form.Group>
-                  <Form.Label>Program:</Form.Label>
-                  <textarea
-                    className='form-control'
-                    rows={EDITOR_ROWS}
-                    value={state.program}
-                    onChange={this.handleProgramChange}
-                    onKeyDown={this.handleProgramKeyDown}
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>&nbsp;</Form.Label>
-                  <Button
-                    variant="primary"
-                    className='btn-block'
-                    type="submit"
-                    onClick={this.handleRunClick}
-                  >
-                    Run
-                  </Button>
-                </Form.Group>
-              </Form>
-            </Col>
-            <Col xs={4}>
-              <Form>
-                <Form.Group>
-                  <Form.Label>Output:</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    readOnly
-                    rows={EDITOR_ROWS}
-                    value={state.output}
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Result:</Form.Label>
-                  <Form.Control
-                    type='text'
-                    readOnly
-                    value={state.result}
-                  />
-                </Form.Group>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-        <footer className="fixed-bottom">
-          <div className="footer-copyright text-center py-1">
-            <span className="text-muted">
+        <section className="hero is-info">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                rufus
+              </h1>
+              <h2 className="subtitle">
+                An experiment about a CEK machine implemented in Rust,
+                compiled to Web Assembly and made alive via Typescript + React.
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-two-thirds">
+                <div className="field">
+                  <label className="label">Program</label>
+                  <div className="control">
+                    <textarea
+                      className="textarea is-family-code"
+                      rows={EDITOR_ROWS}
+                      value={state.program}
+                      onChange={this.handleProgramChange}
+                      onKeyDown={this.handleProgramKeyDown}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">&nbsp;</label>
+                  <div className="control">
+                    <button
+                      className="button is-fullwidth is-info"
+                      onClick={this.handleRunClick}
+                    >
+                      Run
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-one-third">
+                <div className="field">
+                  <label className="label">Output</label>
+                  <div className="control">
+                    <textarea
+                      className="textarea is-family-code"
+                      readOnly
+                      rows={EDITOR_ROWS}
+                      value={state.output}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Result</label>
+                  <div className="control">
+                    <input
+                      className="input is-family-code"
+                      type="text"
+                      readOnly
+                      value={state.result}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer className="footer">
+          <div className="content has-text-centered">
               © 2019 <a href="https://github.com/hurryabit/rufus" target="blank">Martin Huschenbett</a>
-            </span>
           </div>
         </footer>
       </React.Fragment>
