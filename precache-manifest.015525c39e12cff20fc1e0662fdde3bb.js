bcaa645511644175c73cb627278ca03861ebd47a
Martin Huschenbett <martin.huschenbett@posteo.me>
2021-11-10 18:44:03 +0100

    Updates

---
+++ precache-manifest.015525c39e12cff20fc1e0662fdde3bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da405f78668f6784476",
+    "url": "/rufus/2827bfbaef80c01ce862.module.wasm"
+  },
+  {
+    "revision": "45a156819e1da93a1c453c0ad8af87c5",
+    "url": "/rufus/index.html"
+  },
+  {
+    "revision": "975fa6a45148c0959ef3",
+    "url": "/rufus/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "0bc3802572ed94e41637",
+    "url": "/rufus/static/js/2.6f932076.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rufus/static/js/2.6f932076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da405f78668f6784476",
+    "url": "/rufus/static/js/3.a63b831c.chunk.js"
+  },
+  {
+    "revision": "975fa6a45148c0959ef3",
+    "url": "/rufus/static/js/main.5ef241d5.chunk.js"
+  },
+  {
+    "revision": "03a775389b11929593b6",
+    "url": "/rufus/static/js/runtime-main.382bc157.js"
+  }
+]);
